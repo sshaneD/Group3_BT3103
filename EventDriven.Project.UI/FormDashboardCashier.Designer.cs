689722d3036c41a7f4c314df3d65500dd354e2af
sshaneD <delosreyesstefanie9@gmail.com>
2025-09-06 16:05:21 +0800

    User Interface for Login and Dashboard

---
+++ EventDriven.Project.UI/FormDashboardCashier.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace EventDriven.Project.UI
+{
+    partial class FormDashboardCashier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button7 = new Button();
+            panel2 = new Panel();
+            button5 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(11, 143, 172);
+            panel1.Controls.Add(button7);
+            panel1.Location = new Point(-3, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1404, 62);
+            panel1.TabIndex = 1;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(1297, 26);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 3;
+            button7.Text = "Log out";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(0, 61);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(283, 683);
+            panel2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(34, 323);
+            button5.Name = "button5";
+            button5.Size = new Size(206, 46);
+            button5.TabIndex = 6;
+            button5.Text = "Billing";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightGray;
+            button1.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(34, 255);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 46);
+            button1.TabIndex = 2;
+            button1.Text = "Dashboard";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Modern_Minimalist_Black_and_White_Report_Presentation__17_;
+            pictureBox1.Location = new Point(55, 80);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(167, 153);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Modern_Minimalist_Black_and_White_Report_Presentation__19_;
+            pictureBox2.Location = new Point(323, 101);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(1044, 566);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // DashboardCashier
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1399, 708);
+            Controls.Add(pictureBox2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "DashboardCashier";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "DashboardCashier";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button7;
+        private Panel panel2;
+        private Button button5;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
--- EventDriven.Project.UI/FormLogin.cs
+++ EventDriven.Project.UI/FormLogin.cs
@@ -23,42 +23,66 @@ namespace EventDriven.Project.UI
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            try
+            if (txtUsername.Text.Equals("Admin"))
             {
-                   UserModel matchingUser = userController.ValidateUser(txtUsername.Text, txtPassword.Text);
-                   if (matchingUser != null)
-                   {
-                       this.DialogResult = DialogResult.OK;
-                       this.Close(); ;
-                   }
-                   else throw new Exception("Invalid Credentials");
-                
-                //UserModel matchingUser = new UserModel();
-                //using (SqlConnection Hotel = new SqlConnection(CONNECTIONSTRING))
-                //{
-                //    Hotel.Open();
-                //    string query = "SELECT * FROM dbo.[User] WHERE Username ='" + txtUsername.Text + "'AND Password ='" + txtPassword.Text + "'";
-                //    SqlCommand command = new SqlCommand(query, Hotel);
-
-
-                //    SqlDataAdapter adapter = new SqlDataAdapter(command);
-                //    DataTable table = new DataTable();
-                //    adapter.Fill(table);
-                //    if (table.Rows.Count >= 1)
-                //    {
-                //        this.DialogResult = DialogResult.OK;
-                //        this.Close(); ;
-
-
-                //    }
-                //    else { MessageBox.Show("Invalid Credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
-                //}
+                this.Hide();
+                FormDashboard formDashboard = new FormDashboard();
+                formDashboard.ShowDialog();
+            }
+            else if (txtUsername.Text.Equals("Receptionist"))
+            {
+                this.Hide();
+                FormDashboardFront formDashboardFront = new FormDashboardFront();
+                formDashboardFront.ShowDialog();
+            }
+            else if (txtUsername.Text.Equals("Cashier"))
+            {
+                this.Hide();
+                FormDashboardCashier formDashboardCashier = new FormDashboardCashier();
+                formDashboardCashier.ShowDialog();
             }
 
-            catch (Exception EX)
-            {
-                MessageBox.Show(EX.Message);
-            }
+            //try
+            //{
+            //    UserModel matchingUser = userController.ValidateUser(txtUsername.Text, txtPassword.Text);
+            //    if (matchingUser != null)
+            //    {
+            //        this.DialogResult = DialogResult.OK;
+            //        this.Close(); ;
+            //    }
+            //    else throw new Exception("Invalid Credentials");
+
+            //UserModel matchingUser = new UserModel();
+            //using (SqlConnection Hotel = new SqlConnection(CONNECTIONSTRING))
+            //{
+            //    Hotel.Open();
+            //    string query = "SELECT * FROM dbo.[User] WHERE Username ='" + txtUsername.Text + "'AND Password ='" + txtPassword.Text + "'";
+            //    SqlCommand command = new SqlCommand(query, Hotel);
+
+
+            //    SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //    DataTable table = new DataTable();
+            //    adapter.Fill(table);
+            //    if (table.Rows.Count >= 1)
+            //    {
+            //        this.DialogResult = DialogResult.OK;
+            //        this.Close(); ;
+
+
+            //    }
+            //    else { MessageBox.Show("Invalid Credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            //}
+            //}
+
+            //catch (Exception EX)
+            //{
+            //    MessageBox.Show(EX.Message);
+            //}
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
