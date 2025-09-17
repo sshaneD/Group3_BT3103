07409e9afa91cf5218add815cdaa0c70cd6ac1ac
MarioLuwizZ <marluquidem0440@gmail.com>
2025-09-17 13:56:31 +0800

    i add some patient records and tables

--- EventDriven.Project.DB/dbo/Tables/Patient.sql
+++ EventDriven.Project.DB/dbo/Tables/Patient.sql
@@ -1,5 +1,14 @@
 ﻿CREATE TABLE [dbo].[Patient]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[PatientID] int IDENTITY(1000,1) primary key,
+	[FirstName] varchar(50),
+	[LastName] varchar(50),
+	[Age] int,
+	[Gender] varchar(50),
+	[Diagnosis] varchar(50),
+	[RoomNo] int,
+	[GuardianName] varchar(50),
+	[GuardianNo] varchar(50), 
+   
 
 )
