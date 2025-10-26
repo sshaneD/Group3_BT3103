5dbd07d8f3f70ce95d08588c9b7562d56e3ef3ca
Tantanan23 <tristancabasis101@gmail.com>
2025-10-26 15:09:52 +0800

    Reapply "room db"
    This reverts commit 1a8ac2c615e7f1f22e7dc76dc6ef1c343f38789b.

---
+++ EventDriven.Project.DB/dbo/Tables/RoomAssignments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RoomAssignments]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [RoomAssignmentID] INT NULL, 
+    [RoomID] INT NULL, 
+    [PatientID] INT NULL, 
+    [Bed Number] INT NULL
+)
