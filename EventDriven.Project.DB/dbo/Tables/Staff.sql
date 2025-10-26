2956ec079a04919b9392a0f0dfaf7c0f62209ac6
MarioLuwizZ <marluquidem0440@gmail.com>
2025-10-26 16:03:02 +0800

    i create tables for staff and staff assignment

---
+++ EventDriven.Project.DB/dbo/Tables/Staff.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[StaffID] INT NOT NULL PRIMARY KEY, 
+    [DepartmentID] INT NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Role] NVARCHAR(50) NULL
+)
